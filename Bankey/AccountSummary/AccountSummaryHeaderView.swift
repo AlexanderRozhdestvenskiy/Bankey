7b3f10968db439daa1334cf651654fa571064546
Alexander Rozhdestvenskiy <alexrozh1988@gmail.com>
2022-01-21 20:12:56 +0300

    AddXib

---
+++ Bankey/AccountSummary/AccountSummaryHeaderView.swift
@@ -0,0 +1,12 @@
+//
+//  AccountSummaryHeaderView.swift
+//  Bankey
+//
+//  Created by Alexander Rozhdestvenskiy on 21.01.2022.
+//
+
+import UIKit
+
+class AccountSummaryHeaderView: UIView {
+    
+}
