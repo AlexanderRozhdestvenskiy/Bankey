ad72c7da63b711ca3c9be91410dcf9c8614325ca
Alexander Rozhdestvenskiy <alexrozh1988@gmail.com>
2022-01-25 22:53:15 +0300

    AddUnderLine

--- Bankey/AccountSummary/AccountSummaryViewController.swift
+++ Bankey/AccountSummary/AccountSummaryViewController.swift
@@ -19,6 +19,7 @@ class AccountSummaryViewController: UIViewController {
     
     var tableView = UITableView()
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,6 +36,10 @@ extension AccountSummaryViewController {
     private func setupTableView() {
         tableView.delegate = self
         tableView.dataSource = self
+        
+        tableView.register(AccountSummaryCell.self, forCellReuseIdentifier: AccountSummaryCell.reuseID)
+        tableView.rowHeight = AccountSummaryCell.rowHeight
+        tableView.tableFooterView = UIView()
         
         tableView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(tableView)
@@ -59,8 +64,7 @@ extension AccountSummaryViewController {
 extension AccountSummaryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = UITableViewCell()
-        cell.textLabel?.text = banks[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: AccountSummaryCell.reuseID, for: indexPath) as! AccountSummaryCell
         return cell
     }
     
--- Bankey/AccountSummary/Cell/AccountSummaryCell.swift
+++ Bankey/AccountSummary/Cell/AccountSummaryCell.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class AccountSummaryCell: UITableViewCell {
     
+    let typeLabel = UILabel()
+    let underLine = UIView()
+    let nameLabel = UILabel()
+    
+    static let reuseID = "AccountSummaryCell"
+    static let rowHeight: CGFloat = 100
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
@@ -24,10 +31,25 @@ class AccountSummaryCell: UITableViewCell {
 
 extension AccountSummaryCell {
     private func setup() {
+        typeLabel.translatesAutoresizingMaskIntoConstraints = false
+        typeLabel.font = UIFont.preferredFont(forTextStyle: .caption1)
+        typeLabel.adjustsFontForContentSizeCategory = true
+        typeLabel.text = "Account type"
         
+        underLine.translatesAutoresizingMaskIntoConstraints = false
+        underLine.backgroundColor = appColor
+        
+        contentView.addSubview(typeLabel)
+        contentView.addSubview(underLine)
     }
     
     private func layout() {
+        typeLabel.topAnchor.constraint(equalToSystemSpacingBelow: topAnchor, multiplier: 2).isActive = true
+        typeLabel.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2).isActive = true
         
+        underLine.topAnchor.constraint(equalToSystemSpacingBelow: typeLabel.bottomAnchor, multiplier: 1).isActive = true
+        underLine.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2).isActive = true
+        underLine.trailingAnchor.constraint(equalTo: typeLabel.trailingAnchor).isActive = true
+        underLine.heightAnchor.constraint(equalToConstant: 4).isActive = true
     }
 }
