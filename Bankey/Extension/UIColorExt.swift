a6f6e8c1b28b3f6ede9dacc8f1bb88e015c11a68
Alexander Rozhdestvenskiy <alexrozh1988@gmail.com>
2022-03-03 13:22:02 +0300

    SkeletonCell

---
+++ Bankey/Extension/UIColorExt.swift
@@ -0,0 +1,18 @@
+//
+//  UIColorExt.swift
+//  Bankey
+//
+//  Created by Alexander Rozhdestvenskiy on 03.03.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var gradientDarkGrey: UIColor {
+        return UIColor(red: 239 / 255.0, green: 241 / 255.0, blue: 241 / 255.0, alpha: 1)
+    }
+
+    static var gradientLightGrey: UIColor {
+        return UIColor(red: 201 / 255.0, green: 201 / 255.0, blue: 201 / 255.0, alpha: 1)
+    }
+}
